5566d6dc51c4766cde30bdabaee1d520b32e82ac
cafischer <coralinefischer@gmail.com>
2018-05-16 14:03:53 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='circular_statistics',
+      version='0.1',
+      description='Implementation of some circular statistics according to Matlabs Circular Statistics Toolbox',
+      url='https://github.com/cafischer/circular_statistics',
+      author='Caroline Fischer',
+      author_email='coralinefischer@gmail.com',
+      license='',
+      packages=find_packages(),
+      zip_safe=False)
